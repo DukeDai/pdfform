422b58ddc20540f400832db5e1c9fd5bd429138e
Duke Dai <duke.dai.007@gmail.com>
2018-04-08 22:45:59 +0800

    more sample

--- src/main/java/com/dukedai/MainRunner.java
+++ src/main/java/com/dukedai/MainRunner.java
@@ -17,76 +17,103 @@ import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDPushButton;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class MainRunner {
 
     public static void main(String[] args) throws Exception {
+        renderAdvance();
+
+        //renderTemplate(pdfDocument, acroForm);
+    }
+
+    private static void renderAdvance() throws IOException {
+        PDDocument pdfDocument = PDDocument.load(new File("employee.pdf"));
+
+        PDDocumentCatalog docCatalog = pdfDocument.getDocumentCatalog();
+        PDAcroForm acroForm = docCatalog.getAcroForm();
+        renderTextField(acroForm, "date", "2018-04-10");
+        renderTextField(acroForm, "name", "Trump");
+        renderTextField(acroForm, "toYear", "2018-04-10");
+        renderTextField(acroForm, "fromYear", "2011-04-10");
+        renderTextField(acroForm, "EmployerName", "USA");
+        renderImageField(pdfDocument, acroForm, "imageButton", "signature.jpeg");
+        pdfDocument.save("employee_fill.pdf");
+        pdfDocument.close();
+    }
+
+
+    private static void renderSimple() throws IOException {
         PDDocument pdfDocument = PDDocument.load(new File("form_template.pdf"));
 
         PDDocumentCatalog docCatalog = pdfDocument.getDocumentCatalog();
         PDAcroForm acroForm = docCatalog.getAcroForm();
-
         if (acroForm != null) {
-            PDField field = (PDField) acroForm.getField("field1");
-            field.setValue("new field value 1");
+            renderTextField(acroForm, "field1", "new field value 1");
+            renderTextField(acroForm, "field2", "new field value 2");
+            renderTextField(acroForm, "field3", "new field value 3");
 
-            field = (PDField) acroForm.getField("field2");
-            field.setValue("new field value 2");
+            renderImageField(pdfDocument, acroForm, "field4", "signature.jpeg");
 
-            field = (PDField) acroForm.getField("field3");
-            field.setValue("new field value 3");
 
-            PDField buttonField = acroForm.getField("field4");
-            System.out.println(buttonField.getClass().getSimpleName());
-            if (buttonField instanceof PDPushButton) {
-                PDPushButton pdPushButton = (PDPushButton) buttonField;
+            pdfDocument.save("form_fill.pdf");
+            pdfDocument.close();
+        }
+    }
 
-                List<PDAnnotationWidget> widgets = pdPushButton.getWidgets();
-                if (widgets != null && widgets.size() > 0) {
-                    PDAnnotationWidget annotationWidget = widgets.get(0); // just need one widget
+    private static void renderTextField(PDAcroForm acroForm, String name, String value) throws IOException {
+        PDField field = (PDField) acroForm.getField(name);
+        field.setValue(value);
+    }
 
-                    String filePath = "signature.jpeg";
-                    File imageFile = new File(filePath);
+    private static void renderImageField(PDDocument pdfDocument, PDAcroForm acroForm, String name, String value) throws IOException {
+        PDField buttonField = acroForm.getField(name);
+        System.out.println(buttonField.getClass().getSimpleName());
+        if (buttonField instanceof PDPushButton) {
+            PDPushButton pdPushButton = (PDPushButton) buttonField;
 
-                    if (imageFile.exists()) {
-                        /*
-                         * BufferedImage bufferedImage = ImageIO.read(imageFile);
-                         * PDImageXObject pdImageXObject = LosslessFactory.createFromImage(document, bufferedImage);
-                         */
-                        PDImageXObject pdImageXObject = PDImageXObject.createFromFile(filePath, pdfDocument);
-                        float imageScaleRatio = (float) pdImageXObject.getHeight() / (float) pdImageXObject.getWidth();
+            List<PDAnnotationWidget> widgets = pdPushButton.getWidgets();
+            if (widgets != null && widgets.size() > 0) {
+                PDAnnotationWidget annotationWidget = widgets.get(0); // just need one widget
 
-                        PDRectangle buttonPosition = getFieldArea(pdPushButton);
-                        float height = buttonPosition.getHeight();
-                        float width = height / imageScaleRatio;
-                        float x = buttonPosition.getLowerLeftX();
-                        float y = buttonPosition.getLowerLeftY();
+                String filePath = value;
+                File imageFile = new File(filePath);
 
-                        PDAppearanceStream pdAppearanceStream = new PDAppearanceStream(pdfDocument);
-                        pdAppearanceStream.setResources(new PDResources());
-                        try (PDPageContentStream pdPageContentStream = new PDPageContentStream(pdfDocument, pdAppearanceStream)) {
-                            pdPageContentStream.drawImage(pdImageXObject, x, y, width, height);
-                        }
-                        pdAppearanceStream.setBBox(new PDRectangle(x, y, width, height));
+                if (imageFile.exists()) {
+                    /*
+                     * BufferedImage bufferedImage = ImageIO.read(imageFile);
+                     * PDImageXObject pdImageXObject = LosslessFactory.createFromImage(document, bufferedImage);
+                     */
+                    PDImageXObject pdImageXObject = PDImageXObject.createFromFile(filePath, pdfDocument);
+                    float imageScaleRatio = (float) pdImageXObject.getHeight() / (float) pdImageXObject.getWidth();
 
-                        PDAppearanceDictionary pdAppearanceDictionary = annotationWidget.getAppearance();
-                        if (pdAppearanceDictionary == null) {
-                            pdAppearanceDictionary = new PDAppearanceDictionary();
-                            annotationWidget.setAppearance(pdAppearanceDictionary);
-                        }
+                    PDRectangle buttonPosition = getFieldArea(pdPushButton);
+                    float height = buttonPosition.getHeight();
+                    float width = height / imageScaleRatio;
+                    float x = buttonPosition.getLowerLeftX();
+                    float y = buttonPosition.getLowerLeftY();
 
-                        pdAppearanceDictionary.setNormalAppearance(pdAppearanceStream);
-                        System.out.println("Image '" + filePath + "' inserted");
-
-                    } else {
-                        System.err.println("File " + filePath + " not found");
+                    PDAppearanceStream pdAppearanceStream = new PDAppearanceStream(pdfDocument);
+                    pdAppearanceStream.setResources(new PDResources());
+                    try (PDPageContentStream pdPageContentStream = new PDPageContentStream(pdfDocument, pdAppearanceStream)) {
+                        pdPageContentStream.drawImage(pdImageXObject, x, y, width, height);
                     }
+                    pdAppearanceStream.setBBox(new PDRectangle(x, y, width, height));
+
+                    PDAppearanceDictionary pdAppearanceDictionary = annotationWidget.getAppearance();
+                    if (pdAppearanceDictionary == null) {
+                        pdAppearanceDictionary = new PDAppearanceDictionary();
+                        annotationWidget.setAppearance(pdAppearanceDictionary);
+                    }
+
+                    pdAppearanceDictionary.setNormalAppearance(pdAppearanceStream);
+                    System.out.println("Image '" + filePath + "' inserted");
+
+                } else {
+                    System.err.println("File " + filePath + " not found");
                 }
             }
-
-            pdfDocument.save("fill.pdf");
-            pdfDocument.close();
         }
     }
 
@@ -95,4 +122,6 @@ public class MainRunner {
         COSArray fieldAreaArray = (COSArray) fieldDict.getDictionaryObject(COSName.RECT);
         return new PDRectangle(fieldAreaArray);
     }
+
+
 }
